02e44bc87b90e470ea6f606e2fc8c35890ba2e06
Arnold YAYI <87656513+arnold2905@users.noreply.github.com>
2021-12-10 14:32:52 +0100

    cours suite
    cours

---
+++ 01_intro.html/01_introduction.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Bootstrap CSS -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title> CoursPHP - Introduction</title>
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+    <header class="container-fluid-bg-light">
+        <h1 class="display-4">CoursPHP - Introduction</h1>
+        <p class="lead">PHP : PHP Hypertext Processor</p>
+    </header>
+
+    <div class="container bg-white">
+        <section class="row m-2 p-2">
+            <div class="col-md-4 border border-primary">
+                <h4>Réaliser un site dynamique</h4>
+                <p>Pour réaliser un site dynamique nous allons aborder les points suivants:</p>
+                <ul>
+                    <li>La syntaxe et les caractéristiques du langage PHP (v7)</li>
+                    <li>Les notions essentielles du langages SQL permettant la gestion d'une BDD et la réalisation des requêtes de base</li>
+                    <li>... et les moyens d'y accéder à l'aide de fonctions spécialisées de PHP( ou d'objet)</li>
+                </ul>
+            </div>
+            <!-- fin col  -->
+
+            <div class="col-md-4 border border-warning">
+                <h4>Qu'est-ce que PHP?</h4>
+            </div>
+            <!-- fin col  -->
+
+            <div class="col-md-4 border border-danger">
+                <h4>Rappel sur les BDD</h4>
+            </div>
+            <!-- fin col  -->
+        </section>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
+        crossorigin="anonymous"></script>
+</body>
+</html>
